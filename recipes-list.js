05e5bfe610eca6a4fec33ffb3673cf8a50403655
Luca <luca.milanese90@gmail.com>
2021-03-18 11:08:06 +0100

    created recipes.js and recipe-list.js

---
+++ recipes-list.js
@@ -0,0 +1,32 @@
+const recipies = [
+    {
+        country: 'Italy',
+        name: 'another carbonara',
+        ingredients: ['eggs', 'pecorino cheese', 'pasta', 'pepper', 'guanciale'],
+        quantity: [2, 35, 200, 'qb', 80],
+        unitMeasure: ['', 'g', 'g', '', 'g'],
+        portions: 4,
+        picture: 'https://images.unsplash.com/photo-1595908129746-57ca1a63dd4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+        country: 'Italy',
+        name: 'carbonara',
+        ingredients: ['eggs', 'pecorino cheese', 'pasta', 'pepper', 'guanciale'],
+        quantity: [2, 35, 200, 'qb', 80],
+        unitMeasure: ['', 'g', 'g', '', 'g'],
+        portions: 2,
+        picture: 'https://images.unsplash.com/photo-1579631542720-3a87824fff86?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        country: 'Italy',
+        name: 'third recipe',
+        ingredients: ['eggs', 'pecorino cheese', 'pasta', 'pepper', 'guanciale'],
+        quantity: [2, 35, 200, 'qb', 80],
+        unitMeasure: ['', 'g', 'g', '', 'g'],
+        portions: 2,
+        picture: 'https://images.unsplash.com/photo-1614961909013-1e2212a2ca87?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+    }
+];
+
+
+module.exports = recipies;
