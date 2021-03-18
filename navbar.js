231b542a852fcc378102dae2c9799c096d975d88
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-18 12:55:30 +0100

    add basic open/close mechanism for navbar

--- navbar.js
+++ navbar.js
@@ -1,5 +1,48 @@
-const navBar = document.querySelector("#header nav");
+//getting the header element
+const header = document.querySelector("header");
 
+//adding navigation links to header
+const navLinks = document.createElement("ul");
+navLinks.classList.add("nav-links", "nav-closed");
+header.prepend(navLinks);
+
+const navHome = document.createElement("li");
+navHome.classList.add("nav-home");
+navHome.innerHTML = "<a>Home</a>";
+navLinks.appendChild(navHome);
+
+const navRecipe = document.createElement("li");
+navRecipe.classList.add("nav-recipe");
+navRecipe.innerHTML = "<a>Cook at home</a>";
+navLinks.appendChild(navRecipe);
+
+const navRestaurant = document.createElement("li");
+navRestaurant.classList.add("nav-restaurant");
+navRestaurant.innerHTML = "<a>Eat out</a>";
+navLinks.appendChild(navRestaurant);
+
+
+//getting the nav element
+const navBar = document.querySelector("nav");
+
+//adding logo to navBar
 const logo = document.createElement("div");
 logo.classList.add("logo");
 navBar.appendChild(logo);
+
+//adding hamburger to navBar
+const hamburger = document.createElement("div");
+hamburger.classList.add("hamburger", "nav-closed");
+navBar.appendChild(hamburger);
+
+//adding open-close mechanism
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("nav-closed");
+    navLinks.classList.toggle("nav-closed");
+});
+
+navLinks.addEventListener("click", function () {
+    hamburger.classList.toggle("nav-closed");
+    navLinks.classList.toggle("nav-closed");
+});
