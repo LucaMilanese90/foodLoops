b6611e1a6bf818d7cd00b27a95c7666060a1f96e
alialouies <makarona.aliki@gmail.com>
2021-03-24 11:52:19 +0100

    making navigation finally work

--- entry.js
+++ entry.js
@@ -1,13 +1,25 @@
 const countrySection = document.getElementById('country-selection');
+const countries = document.querySelectorAll('.country-card > a');
 const inOut = document.getElementById('in-out-selection');
+let cuisineType;
 
-function logVar (a, b) {
-    console.log(a,b)
+function logVar(a, b) {
+    cuisineType = b;
+    console.log(a, b)
+    // a === 'in' ? window.location.href='recipes.html' : window.location.href='restaurant.html';
+
 }
 
 inOut.addEventListener('click', event => {
     inOrOut = event.target.className;
     countrySection.scrollIntoView({behavior: 'smooth'});
+    countries.forEach((country) => {
+        if(inOrOut === 'in') {
+            country.href = './recipes.html'
+        } else {
+            country.href = './restaurants.html'
+        }
+    })
 })
 
 countrySection.addEventListener('click', event => {
