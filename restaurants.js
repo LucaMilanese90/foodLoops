a91996b559a85195dc4f73d4d531959dc2adee9a
Angel Sámano Hamud <Angelsam@air-de-angel.localdomain>
2021-03-24 17:21:18 +0100

    Changes Restaurant

--- restaurants.js
+++ restaurants.js
@@ -1,48 +1,124 @@
-const restaurantsCountry = ["italy","greece", "mexico", "germany","syrian"]
+const mexico = [ {
 
-const countries = [ {
   country: "Mexico",  
   name: "Maria Bonita",
-  about: "Berlin‘s first authentic Mexican Bistro opened in 2009 and set the start of the Mexican Food–Revolution in Berlin. Ever since people enjoy traditionally prepared dishes, hand-made salsas and Margaritas just like in Mexico. So have a bite, close your eyes and take a little trip “south of the border”.",
-  image: "./src.images/Maria Bonita.jpg",
-  mapUrl: "https://maps.google.com/maps?q=Maria%20Bonita,%20Danziger%20Str.%2033,%2010435%20Berlin&t=&z=13&ie=UTF8&iwloc=&output=embed"
+  about: "Berlin‘s first authentic Mexican Bistro opened in 2009 and set the start of the Mexican Food–Revolution in Berlin. Ever since people enjoy traditionally prepared dishes, hand-made salsas and Margaritas just like in Mexico. So have a bite, close your eyes and take a little trip “south of the border.",
+  imageUrl: "./src.images/Maria Bonita.jpg",
+  mapUrl: "https://maps.google.com/maps?q=maria%20bonita%20restaurant%20berlin&t=&z=13&ie=UTF8&iwloc=&output=embed",
+  buttonUrl: "https://www.mariabonita.de/"
 },
+
 {
   country: "Mexico",  
   name: "Zapata",
   about: "Lorem50",
-  image: "./src.images/Zapata.jpg",
-  mapUrl: "https://maps.google.com/maps?q=Hauptstra%C3%9Fe%205,%2010827%20Berlin&t=&z=13&ie=UTF8&iwloc=&output=embed"
+  imageUrl: "./src.images/Zapata.jpg",
+  mapUrl: "https://maps.google.com/maps?q=zapata%20restaurant%20berlin&t=&z=13&ie=UTF8&iwloc=&output=embed",
+  buttonUrl: "http://zapata-berlin.de/"
 },
+
 {
-country: "Mexico",  
+  country: "Mexico",  
   name: "Tacoriño",
   about: "Lorem50",
-  image: "./src.images/Tacoriño .jpg",
-  mapUrl: "https://maps.google.com/maps?q=TACORI%C3%91O,%20Knesebeckstra%C3%9Fe,%20Berl%C3%ADn&t=&z=13&ie=UTF8&iwloc=&output=embed"
+  imageUrl: "./src.images/Tacoriño .jpg",
+  mapUrl: "https://maps.google.com/maps?q=tacori%C3%B1o%20restaurant%20berlin&t=&z=13&ie=UTF8&iwloc=&output=embed",
+  buttonUrl: "https://www.tacorino.de/" 
 }
 ]
+
+
+
+
+
+
+
+
+
+// function createCard (name, description, restName, restImg, restDesc, restMap, restLink){
+
+// //const card = document.querySelector("#main-restaurant");
+// //card.classList.add("card"); 
+
+
+
+// const countryName = document.querySelector(".country-title");
+// //document.createElement("div"); 
+// //card.classList.add("country-name");
+// countryName.innerText = name;
+// card.appendChild(countryName); 
+
+// const countryDescription = document.querySelector(".country-description");
+// //document.createElement("div"); 
+// //card.classList.add("country-description"); 
+// countryDescription.innerText = description; 
+// //card.appendChild(countryDescription); 
+
+// const restaurantName = document.querySelector(".restaurant-title"); 
+// //card.classList.add("restaurant-name");
+// restaurantName.innerText = restName;  
+// //card.appendChild(restaurantName); 
+
+// const restaurantImage = document.querySelector("#imgRestaurant"); 
+// restaurantImage.style.backgroundImage = `url(${restImg})`;
+// //restaurantImage.classList.add("restaurant-image"); 
+// restaurantImage.src = restImg; 
+// //card.appendChild(restaurantImage); 
+
+// const restaurantDescription = document.querySelector(".restaurant-description"); 
+// //card.classList.add("restaurant-description"); 
+// restaurantDescription.innerText = restDesc; 
+// //card.appendChild(restaurantDescription); 
+
+// const map = document.querySelector("#gmap_canvas1"); 
+// //card.classList.add("map"); 
+// map.src = restMap;
+// //card.appendChild(map); 
+
+// const cardbutton = document.querySelector("#buttonLink");
+// //card.classList.add("button-website"); 
+// cardbutton.href = restLink; 
+// cardbutton.innerText = "Go to Website";    
+// //const container = document.querySelector("#main-restaurant"); 
+// //container.appendChild(card); 
+// }
+
+// for (let i = 0; i < mexico.length; i++) { 
+// createCard(mexico[i].name, mexico[i].description, mexico[i].restName, mexico[i].restImg, mexico[i].restDesc, mexico[i].restMap, mexico[i].restLink);
+// }
+
+// for (let j = 0; j < italy.length; i++){
+//   createCard(italy[j].name, italy[j].description, italy[j].restName, italy[j].restImg, italy[j].restDesc, italy[j].restMap, italy[j].restLink);}
+
+
+
  
- const mainMap = document.body.querySelector("#gmap_canvas1");
-mainMap.src = countries[0].mapUrl; 
-
-const imageR = document.body.querySelector("#maria");
-imageR.src = countries[0].image;
-
-function arrayRestaurants(array){
-  for (let i = 0; i < countries.length; i ++){
-    console.log(array[i]); 
-  }
-}
 
 
 
 
 
-
-
-
-
+// function updateSecondaryImageAndTitle(recipeIndex) {
+//     const moreRecipes = document.body.querySelector('#more-recipes');
+//     const alternativeRecipes = moreRecipes.querySelectorAll('.alternative-recipes');
+//     let i = 1;
+//     alternativeRecipes.forEach(item => {
+//         item.querySelector('img').src = recipes[(recipeIndex + i) % recipes.length].picture;
+//         item.querySelector('h3').innerHTML = recipes[(recipeIndex + i) % recipes.length].name;
+//         i++;
+//     })
+// }
+// function updateMainImageAndTitle(recipeIndex) {
+//     const ingredientListDiv = document.body.querySelector('#recipe-main');
+//     const mainImage = ingredientListDiv.querySelector('img');
+//     mainImage.src = recipes[recipeIndex].picture;
+//     mainImage.alt = recipes[recipeIndex].name;
+//     const mainTitle = ingredientListDiv.querySelector('#recipe-main-title');
+//     mainTitle.innerHTML = recipes[recipeIndex].name;
+// }
+// function UpdateRecipeIndex() {
+//     recipes.findIndex(x => x.name === recipeName);
+// };
 
 
 
